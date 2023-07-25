53c6a0356e60cfe8cba7d3c8e9e8f77957a9d0b7
mv_taliani <mv.taliani@gmail.com>
2023-07-25 15:23:15 -0300

    Encurtador de urls configurado

---
+++ src/cliente.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, current_app, g, abort
+from src.models import Links
+
+
+lead = Blueprint('lead', __name__)
+
+
+@lead.url_value_preprocessor
+def get_cliente(endpoint, values):
+    hashh = values.pop('hashdd')
+    id = current_app.hashid.decode(hashh)
+    link = Links.query.get_or_404(id[0])
+    g.cliente = link.cliente
+
+
+@lead.get('/')
+def oi():
+    return f'{g.cliente.cpf}'
+
+
+def configure(app):
+    app.register_blueprint(lead, url_prefix='/<hashdd>')
